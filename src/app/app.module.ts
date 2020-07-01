90ffa10b84eb0e1f027ede37c36fdcf7c43d1579
tony <tonycs_y@126.com>
2020-07-01 15:03:20 +0800

    添加angular in memory web api来模拟api请求

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import {InMemoryDataService } from "./in-memory-data.service";
+
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -11,6 +14,9 @@ import {AccountModule} from "./pages/account/account.module";
         AppComponent
     ],
     imports: [
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService, {dataEncapsulation: false},
+        ),
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {InMemoryDbService} from "angular-in-memory-web-api";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService{
+
+    createDb() {
+        const userList = [
+            {
+                name: 'tony',
+                password: '123456',
+            }
+        ];
+        return {
+            userList,
+        }
+    }
+}
