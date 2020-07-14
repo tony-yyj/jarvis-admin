e3a66b0cc30d6801971351e9403bba7c3724a2e1
tony <tonycs_y@126.com>
2020-07-14 11:16:32 +0800

    form group 探索

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import { IndexComponent } from './index/index.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
 
 const router: Routes = [
     {
@@ -13,9 +16,12 @@ const router: Routes = [
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [
-    CommonModule,
-      RouterModule.forChild(router),
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(router),
+        ReactiveFormsModule,
+        MatInputModule,
+        MatButtonModule,
+    ]
 })
 export class DashboardModule { }
--- src/app/pages/dashboard/index/index.component.ts
+++ src/app/pages/dashboard/index/index.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+    form: FormGroup;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private _fb: FormBuilder,
+    ) {
+    }
+
+    ngOnInit(): void {
+        this.form = this._fb.group({
+            peoples: this._fb.array([
+            ])
+        })
+    }
+
+    get peoples(): FormArray {
+        return this.form.get('peoples') as FormArray;
+    }
+
+    addPeople() {
+        this.peoples.push(this._fb.group({
+            name: null,
+            phone: null,
+        }));
+    }
+
+    deletePeople(i: number) {
+        this.peoples.removeAt(i);
+    }
+
+    submit() {
+        const values = this.form.getRawValue();
+        console.log(values)
+    }
 
 }
