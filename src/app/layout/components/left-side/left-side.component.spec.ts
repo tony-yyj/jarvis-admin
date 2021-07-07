c6cc95cba38c8b243f86e7e006b69a97698f753a
tony <tonycs_y@126.com>
2021-07-07 17:08:01 +0800

    update angular/core

--- src/app/layout/components/left-side/left-side.component.spec.ts
+++ src/app/layout/components/left-side/left-side.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LeftSideComponent } from './left-side.component';
 
@@ -6,7 +6,7 @@ describe('LeftSideComponent', () => {
   let component: LeftSideComponent;
   let fixture: ComponentFixture<LeftSideComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LeftSideComponent ]
     })
